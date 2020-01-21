34f406a1aed5880fa2ced2b7c0324aac331608d6
liu <liuhz@idss-cn.com>
2020-01-21 16:11:21 +0800

    source optimization;

--- lombok-demo/src/main/java/pers/wilson/lombok/demo/LombokApplication.java
+++ lombok-demo/src/main/java/pers/wilson/lombok/demo/LombokApplication.java
@@ -3,9 +3,9 @@ package pers.wilson.lombok.demo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Application {
+public class LombokApplication {
 
-    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+    private static final Logger logger = LoggerFactory.getLogger(LombokApplication.class);
 
     public static void main(String[] args) {
         logger.info("info~~");
