e0d4b24d398fdd5db30ca207525a50a9c1e040b1
liu <liuhz@idss-cn.com>
2020-07-21 14:10:24 +0800

    update logback-demo;

---
+++ logback-demo/README.md
@@ -0,0 +1,2 @@
+# logback-demo
+使用slf4j+logback输出日志demo
--- logback-demo/src/main/java/pers/wilson/logback/demo/LogbackApplication.java
+++ logback-demo/src/main/java/pers/wilson/logback/demo/LogbackApplication.java
@@ -1,11 +1,11 @@
-package pers.wilson.lombok.demo;
+package pers.wilson.logback.demo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class LombokApplication {
+public class LogbackApplication {
 
-    private static final Logger logger = LoggerFactory.getLogger(LombokApplication.class);
+    private static final Logger logger = LoggerFactory.getLogger(LogbackApplication.class);
 
     public static void main(String[] args) {
         logger.info("info~~");
